1d2ddfdde7c4c2397cd686a78df9f4f7bba187b9
Maxim Makarov <makarovmm@yandex.ru>
2017-05-09 20:10:14 +0700

    Complete provider methods

--- app/src/main/java/maximmakarov/anylist/storage/DataProvider.kt
+++ app/src/main/java/maximmakarov/anylist/storage/DataProvider.kt
@@ -10,18 +10,20 @@ import io.requery.sql.KotlinEntityDataStore
 import io.requery.sql.TableCreationMode
 import maximmakarov.anylist.item.Item
 import maximmakarov.anylist.item.Models
+import java.util.*
 
 /**
  * @author Maxim Makarov
  * @since 05.05.2017
  */
 class DataProvider {
-    val dbVersion: Int = 1
+    val DB_VERSION: Int = 1
+    val ABSENT_PARENT_ID = 0
 
     val data: KotlinReactiveEntityStore<Persistable>
 
     constructor(context: Context) {
-        val source = DatabaseSource(context, Models.DEFAULT, dbVersion)
+        val source = DatabaseSource(context, Models.DEFAULT, DB_VERSION)
         source.setTableCreationMode(TableCreationMode.DROP_CREATE)
         data = KotlinReactiveEntityStore<Persistable>(KotlinEntityDataStore(source.configuration))
     }
@@ -34,8 +36,18 @@ class DataProvider {
         return (data select (Item::class) where (Item::parentId eq item.id)).get()
     }
 
-    fun getPathToItem(item: Item) {
-
+    fun getPathToItem(i: Item) {
+        //todo try to optimize without loading all items
+        val items = loadAllItems()
+        val list = ArrayList<Item>()
+        var item = i
+        list.add(item)
+        while (item.parentId != ABSENT_PARENT_ID) {
+            val parent = items.first { item.parentId == it.id }
+            list.add(parent)
+            item = parent
+        }
+        list.reversed()
     }
 
     fun saveItem(item: Item) {
@@ -46,8 +58,24 @@ class DataProvider {
         data update item
     }
 
-    fun removeItemSafe(item: Item) { //with all sub-hierarchy
-        //todo rewrite
+    fun deleteItem(item: Item) {
         data delete item
+    }
+
+    /** remove item with all sub-hierarchy */
+    fun removeItemSafe(item: Item) {
+        getAllSubItems(item).forEach { deleteItem(it) }
+        deleteItem(item)
+    }
+
+    fun getAllSubItems(item: Item): List<Item> {
+        val subItems = ArrayList<Item>()
+        val childs = loadChilds(item)
+
+        subItems.addAll(childs)
+        for (child in childs) {
+            subItems.addAll(getAllSubItems(child))
+        }
+        return subItems
     }
 }
